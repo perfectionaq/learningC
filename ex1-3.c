6085617aa7767f676a6d52f906935e7f2f22a842
perfectionaq <perfection.a.q@gmail.com>
2023-12-20 23:02:37 +0300

    exercise 1-3

---
+++ ex1-3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    float fahr, celsius;
+    float lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+
+    printf("Fahrenheit to Celsius\n");
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%3.0f\t%6.1f\n", fahr, celsius);
+        fahr = fahr + step;
+    }
+
+    return 0;
+}
