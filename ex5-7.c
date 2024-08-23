72caa72282e9cf8b0f4805f7566e7ce5a17b8970
perfectionq <perfection.a.q@gmail.com>
2024-08-23 17:28:41 +0300

    exercise 5-7

---
+++ ex5-7.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAXLINES 5000
+
+char *lineptr[MAXLINES];
+
+int readlines(char *lineptr[], int nlines);
+void writelines(char *lineptr[], int nlines);
+
+void qsort(char *lineptr[], int left, int right);
+
+int main()
+{
+	int nlines;
+
+	if ((nlines = readlines(lineptr, MAXLINES)) > 0) {
+		qsort(lineptr, 0, nlines-1);
+		writelines(lineptr, nlines);
+		return 0;
+	} else {
+		printf("error: inptu too big to sort\n");
+		return 1;
+	}
+}
+
+#define MAXLEN 1000
+// These are extern since they're being used from other files 
+extern int getlinee(char *, int);
+extern char *alloc(int);
+
+int readlines(char *lineptr[], int maxlines)
+{
+	int len, nlines;
+
+	nlines = 0;
+	while ((len = getlinee(lineptr[nlines++], MAXLEN) ) > 0)
+		if (nlines >= maxlines)
+			return -1;
+	return nlines;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+	int i;
+	while (nlines-- > 0)
+		printf("%s\n", *lineptr++);
+}
+
+void qsort(char *v[], int left, int right)
+{
+	int i, last;
+	void swap(char *v[], int i, int j);
+
+	if (left >= right)
+		return;
+	swap(v, left, (left + right) / 2);
+	last = left;
+
+	for (i = left+1; i <= right; i++)
+		if (strcmp(v[i], v[left]) < 0)
+			swap(v, ++last, i);
+	swap(v, left, last);
+	qsort(v, left, last-1);
+	qsort(v, last+1, right);
+}
+
+void swap(char *v[], int i, int j)
+{
+	char *temp;
+
+	temp = v[i];
+	v[i] = v[j];
+	v[j] = temp;
+}
